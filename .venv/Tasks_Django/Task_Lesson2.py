35a67c109148daa1bf7dd3434e6a83f38f58de84
Viktor Shkliar <shklyar.victor@gmail.com>
2023-08-31 16:14:48 +0300

    Upd20230831

--- .venv/Tasks_Django/Task_Lesson2.py
+++ .venv/Tasks_Django/Task_Lesson2.py
@@ -38,7 +38,7 @@
 # import re
 
 # def validate_email(email):
-#     pattern = r'^[a-zA-Z0-9]+[a-zA-Z0-9_-]*@[a-zA-Z0-9]+\.[a-zA-Z]+$'
+#     pattern = r'^[a-zA-Z0-9]+[a-zA-Z0-9_-]*@[a-zA-Z0-9]+\.[a-zA-Z]+\.[a-zA-Z]+$'
     
 #     if re.match(pattern, email):
 #         if email.count('-') > 1 or (email.count('-') == 1 and email[0] == '-'):
